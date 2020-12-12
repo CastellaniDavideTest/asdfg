9b8635b615521af5260718096d27631c161f2a69
Davide Castellani <38205382+DavideC03@users.noreply.github.com>
2020-12-12 17:12:47 +0100

    Created bin/asdfg.py

---
+++ bin/asdfg.py
@@ -0,0 +1,19 @@
+"""asdfg
+"""
+
+__author__ = "help@castellanidavide.it"
+__version__ = "1.0 2020-12-12"
+
+class asdfg:
+	def __init__ (self):
+		"""Where it all begins
+		"""
+		print(asdfg.asdfg())
+	
+	def asdfg():
+		"""asdfg first funtion
+		"""
+		return "asdfg" #for the test (see test_asdfg.py)
+		
+if __name__ == "__main__":
+	asdfg()
